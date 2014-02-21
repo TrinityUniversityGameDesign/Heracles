0633b7f13f0b190ec1b41d58a6af80d26cad8e71
kingofferrets <cfisher@trinity.edu>
2014-02-21 11:44:34 -0600

    ArrowCollisionScript Added
    Anything that needs to collide with arrows should use the
    ArrowCollisionScript, or if it needs to do something unusual on
    collision, a variant of it.

---
+++ Assets/Scenes/Examples/BowFireTest/Assets/Scripts/ArrowCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowCollision : MonoBehaviour {
+    //This will destroy the arrow when it impacts an object. Use this script for anything that needs to collide with arrows!
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+           Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+        }
+    }
+}
