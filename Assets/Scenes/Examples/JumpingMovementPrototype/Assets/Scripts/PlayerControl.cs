2e2f0816e63a777e7e77ea764c848635812b1721
Carbohydron <colson1@trinity.edu>
2014-02-21 15:05:39 -0600

    Jumping & Movement Prototype Bug Fix
    Accidental edge cases resulting in a double jump were fixed, as well as
    some timing issues. Jumping should respond to input better.

--- Assets/Scenes/Examples/JumpingMovementPrototype/Assets/Scripts/PlayerControl.cs
+++ Assets/Scenes/Examples/JumpingMovementPrototype/Assets/Scripts/PlayerControl.cs
@@ -4,58 +4,40 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 
-	bool facingRight = true;
+//	bool facingRight = true;
 	public Vector2 resetPosition = new Vector2 (-7.5f, -1.75f);
 
 	public float walkSpeed;
 	public float runSpeed;
 	
 	public Transform groundCheck;
-	float groundRadius = 0.2f;
+	float groundRadius = 0.3f;
 	bool grounded = false;
 	public LayerMask groundMask;
 	public float jumpPower;
 	public string horizAxisName = "Horizontal";
 	public string jumpAxisName = "Vertical";
 
-	bool dead = false;
+//	bool dead = false;
 
-	Animator anim;
-
-
+//	Animator anim;
 	void Awake() {
-		anim = GetComponent<Animator> ();
-	}
-
-	public void Reset() {
-		transform.position = resetPosition;
-
-		grounded = false;
+//		anim = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundMask);
+		//anim.SetBool ("Grounded", grounded);
+		bool jump = Input.GetButtonDown (jumpAxisName);
+		if (jump && grounded) {
+			rigidbody2D.AddForce (new Vector2 (0, jumpPower));
+		}
 	}
-
-
+	
 	void FixedUpdate () {
-			float inputX = Input.GetAxis (horizAxisName);
-			float vel = inputX * runSpeed;
-			rigidbody2D.velocity = new Vector2 (vel, rigidbody2D.velocity.y);
-
-			grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundMask);
-			anim.SetBool ("Grounded", grounded);
-			bool jump = Input.GetButtonDown (jumpAxisName);
-			if (jump && grounded) {
-				rigidbody2D.AddForce (new Vector2 (0, jumpPower));
-			}
-		
+		float inputX = Input.GetAxis (horizAxisName);
+		float vel = inputX * runSpeed;
+		rigidbody2D.velocity = new Vector2 (vel, rigidbody2D.velocity.y);
 	}
-
-	void OnTriggerEnter2D ( Collider2D other ) {
-
-
-	}
-
 }
