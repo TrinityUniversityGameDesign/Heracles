4ba3fa16d8b120f0ec340ca31f4411e3d5745a10
kingofferrets <cfisher@trinity.edu>
2014-02-26 06:03:38 -0600

    Added environment prefabs; new movement bug
    Since Hayden is having trouble pushing to Master, I incorporated the
    environment prefabs into my copy for pushing. Can people test movement
    for me? I added a moving platform to the jumping and movement prototype,
    and when I jump into the side of it while it's moving, I start getting
    some really weird glitches about forces acting on the player that
    shouldn't be occurring.

---
+++ Assets/Scenes/LaddersAndPlatforms/Assets/Scripts/MoveTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTest : MonoBehaviour {
+	public string pathName;
+	public double time;
+	public double delay;
+	public string easetype;
+	public string looptype;
+
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath (pathName), "time", time, "looptype", looptype, "easetype", easetype, "delay", delay));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scenes/LaddersAndPlatforms/Assets/Scripts/MovingPlatformTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		other.transform.parent = gameObject.transform;
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		other.transform.parent = null;
+	}
+}
