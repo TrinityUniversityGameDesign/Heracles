d3382f1d5560fe597789b1f5e4192553b41fcb97
HaydenHuff2 <hhuffine@trinity.edu>
2014-02-28 12:32:52 -0600

    Camera script + new Prefabs
    In the testing grounds script folder is the cameraFollow script. Drag it
    onto your camera object or select it from a list, and then set the MAX
    and MIN for x and y, mine are 999 or -999 currently. You can then adjust
    the smoothing/ect to suit your needs.
    
    New Prefabs: Pressure Plates, Crates, and Barrels

---
+++ Assets/Scenes/Testing grounds/Scripts/PressurePlate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+	public GameObject playerObject;
+	public int isPressed = 0;
+	public float speed = 1f;
+	public float tick = .1f;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = GameObject.FindWithTag("P1");
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.rigidbody2D.mass >= 1) {
+			isPressed++;
+			//other.gameObject.rigidbody2D.velocity = new Vector2(0, 0);
+			gotPressed();
+				}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+			isPressed -= 1;
+		gotReleased (); 
+	}
+
+	void gotPressed() {
+				if (isPressed == 1) {
+					gameObject.renderer.material.color = Color.grey; 
+				}
+	}
+	void gotReleased(){
+		if (isPressed == 0) {
+					gameObject.renderer.material.color = Color.yellow; 
+
+				}
+		}
+}
