312e35bb760a153fb4f434a7265921b2c9b55acc
ahowarth <ahowarth@trinity.edu>
2014-03-02 10:19:40 -0600

    Added PickUp Script

---
+++ Assets/PickUpScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpScript : MonoBehaviour {
+
+	public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = GameObject.FindWithTag("P1");
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		Destroy(gameObject);
+	
+	}
+}
