81faf85da4f005f85c3dac8eba1127d159929aa9
cjpeake9 <cpeake@trinity.edu>
2014-03-02 12:59:41 -0600

    Added scene for red team, fixed some other issues with moving platforms

--- Assets/Scenes/Testing grounds/Scripts/LadderScript.cs
+++ Assets/Scenes/Testing grounds/Scripts/LadderScript.cs
@@ -6,10 +6,12 @@ public class LadderScript : MonoBehaviour {
 	public GameObject playerObject;
 	bool canClimb = false;
 	public float speed = 1f;
+	float grav = 1;
 
 	// Use this for initialization
 	void Start () {
 		playerObject = GameObject.FindWithTag("P1");
+		grav = playerObject.rigidbody2D.gravityScale;
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
@@ -21,18 +23,21 @@ public class LadderScript : MonoBehaviour {
 	void OnTriggerExit2D (Collider2D other) {
 		if (other.gameObject == playerObject) {
 						canClimb = false;
-						playerObject.rigidbody2D.gravityScale = 1;
+						playerObject.rigidbody2D.gravityScale = grav;
 				}
 	}
 	// Update is called once per frame
 	void Update () {
 		if (canClimb) {
-			if(Input.GetKey(KeyCode.Q)) {
+			//playerObject.gameObject.GetComponent<PlayerControl>().enabled = false;
+			if(Input.GetKey(KeyCode.W)) {
 				playerObject.transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime*speed);
 			}
-			if(Input.GetKey (KeyCode.E)) {
+			if(Input.GetKey (KeyCode.S)) {
 				playerObject.transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime*speed);
 			}
 		}
+		//else
+			//playerObject.gameObject.GetComponent<PlayerControl>().enabled = true;
 	}
 }
