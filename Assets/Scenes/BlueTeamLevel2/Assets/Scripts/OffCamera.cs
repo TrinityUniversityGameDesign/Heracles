a74ace3b20da53959b4bde9103b37d3855a81547
johnnywendy <johnnywendy@gmail.com>
2014-03-02 15:35:59 -0600

    Blue Team Level Prototype v0.1

---
+++ Assets/Scenes/BlueTeamLevel2/Assets/Scripts/OffCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffCamera : MonoBehaviour {
+	
+	public PlayerControl pc;
+	public Camera cp;
+	float cameraX;
+	public float newCX;
+	public float newCY;
+
+
+	//Use this for initialization
+	void Start () {
+		pc.jumpPower = 0f;
+
+		cameraX = cp.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+
+		float playerX = transform.position.x;
+
+		if(playerX > cameraX) {
+			cp.orthographicSize = 5;
+			pc.jumpPower = 400f;
+			cp.transform.position.Set(newCX, newCY, cp.transform.position.z);
+		}
+	}
+}
