f13cfcd1ad679c05931a414c337101cead0aa1f5
kingofferrets <cfisher@trinity.edu>
2014-03-03 11:25:08 -0600

    Finished Green Machine Scene 2
    Other than death. But nobody has death yet, so is k.

--- Assets/Scenes/GreenTeam/StalactiteArrowCollisionScriptA.cs
+++ Assets/Scenes/GreenTeam/StalactiteArrowCollisionScriptA.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class StalactiteArrowCollisionScriptA : MonoBehaviour {
 	Rigidbody2D beep;
+    Transform pos;
+    Vector3 origPos;
+    int counter;
+    bool counting;
 	public AudioClip stalacFall;
+    public int timeToDisappear;
 
 	void Start()
 	{
 		beep = this.gameObject.GetComponent<Rigidbody2D>();
+        pos = this.gameObject.GetComponent<Transform>();
+        origPos = pos.position;
+        counting = false;
+        counter = 0;
 	}
 
 	void OnTriggerEnter2D(Collider2D otherCollider)
@@ -18,8 +27,9 @@ public class StalactiteArrowCollisionScriptA : MonoBehaviour {
 		if (water != null) {
 			beep.drag = 10;
 			beep.gravityScale = 0.2f;
+            counting = true;
 		}
-		else if (shot != null)
+		else if (shot != null && !counting)
 		{
 			Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
 			beep.gravityScale = 1;
@@ -27,6 +37,23 @@ public class StalactiteArrowCollisionScriptA : MonoBehaviour {
 			audio.PlayOneShot(stalacFall);
 		}
 	}
+
+    void FixedUpdate()
+    {
+        if (counting)
+        {
+            counter += 1;
+        }
+        if (counter > timeToDisappear)
+        {
+            pos.position = origPos;
+            counting = false;
+            beep.drag = 0;
+            beep.gravityScale = 0;
+            counter = 0;
+            beep.velocity = new Vector3(0, 0, 0);
+        }
+    }
 }
 
 
