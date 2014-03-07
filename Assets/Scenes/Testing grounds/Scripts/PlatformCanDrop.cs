7605fcba57db73b5b53920078f9a5fd5c81c4e3e
HaydenHuff2 <hhuffine@trinity.edu>
2014-03-07 12:03:53 -0600

    Added Drop Down Platform Prefab
    Also added subfolders to Red Team folder and moved everything inside

---
+++ Assets/Scenes/Testing grounds/Scripts/PlatformCanDrop.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformCanDrop : MonoBehaviour {
+	
+	public GameObject playerObject;
+	public bool canDrop = false;
+	public bool dropping = false;
+	public string horizAxisName = "Horizontal";
+	public string jumpAxisName = "Vertical";
+	public float vertAx = 0.0f;
+	public bool vertHeldDwn;
+	public bool isColliding = false;
+
+	void Awake() {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		vertAx = Input.GetAxis (jumpAxisName); 
+		bool drop = false;
+		if (Input.GetButtonDown (jumpAxisName))
+			vertHeldDwn = true;
+		else
+			vertHeldDwn = false; 
+		
+		if(Input.GetAxis (jumpAxisName) < 0)
+			drop = true;
+		
+		 if (drop && canDrop) {
+			Drop();
+		}
+	}
+
+	void Drop(){
+		dropping = true;
+		gameObject.collider2D.enabled = false;
+	}
+	void StopDrop(){
+		gameObject.collider2D.enabled = true;
+		dropping = false; 
+	}
+	
+	void Start () {
+		playerObject = GameObject.FindWithTag("P1");
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		isColliding = true;
+		if (other.gameObject == playerObject) {
+			canDrop = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		isColliding = false;
+		if (other.gameObject == playerObject) {
+			canDrop = false;
+			if(dropping)
+				StopDrop();
+		}
+	}
+}
