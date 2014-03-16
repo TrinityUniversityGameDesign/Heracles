066c75131da982e9fe98a98fdb95feffda488d1e
addisn <addison.freeman@trinity.edu>
2014-03-16 17:26:50 -0500

    testing audio in Testing Grounds
    audio doesn't play yet though

---
+++ Assets/Scenes/Testing grounds/audioScripts/dragAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragAudio : MonoBehaviour {
+	public AudioClip dragSound;
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnCollisionEnter () {
+		audio.clip = dragSound;
+		audio.Play();
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
