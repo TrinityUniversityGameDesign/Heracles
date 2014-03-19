01474e19c3197b7839f44d9639437b8a28e5dfbb
cunger1 <cunger1@trinity.edu>
2014-03-19 04:52:38 -0500

    Crouch
    Finally committed the crouch functionality I made.

--- Assets/Scenes/Examples/JumpingMovementPrototype/Assets/Scripts/PlayerControl.cs
+++ Assets/Scenes/Examples/JumpingMovementPrototype/Assets/Scripts/PlayerControl.cs
@@ -9,6 +9,8 @@ public class PlayerControl : MonoBehaviour {
 
 	public float walkSpeed;
 	public float runSpeed;
+	public float crouchSpeed;
+	public float crouchHeight = .5f;
 	
 	public Transform groundCheck;
 	float groundRadius = 0.3f;
@@ -17,24 +19,44 @@ public class PlayerControl : MonoBehaviour {
 	public float jumpPower;
 	public string horizAxisName = "Horizontal";
 	public string jumpAxisName = "Vertical";
+	public bool isCrouched = false;
+
+	public Component boxcollider;
+
+	BoxCollider2D bc = new BoxCollider2D();
 
 //	bool dead = false;
 
 //	Animator anim;
 	void Awake() {
 //		anim = GetComponent<Animator> ();
+		bc = GetComponent<BoxCollider2D>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundMask);
-		//anim.SetBool ("Grounded", grounded);
-		bool jump = Input.GetButtonDown (jumpAxisName);
-		if (jump && grounded) {
-			rigidbody2D.AddForce (new Vector2 (0, jumpPower));
+				grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, groundMask);
+				//anim.SetBool ("Grounded", grounded);
+				bool jump = Input.GetButtonDown (jumpAxisName);
+				if (jump && grounded) {
+						rigidbody2D.AddForce (new Vector2 (0, jumpPower));
+				}
+				bool crouch = Input.GetKey (KeyCode.X);
+				if (crouch) {
+						if (!isCrouched) {
+								isCrouched = true;
+								walkSpeed = crouchSpeed;
+								//BoxCollider2D = new BoxCollider2D (BoxCollider2D.size.x, crouchHeight);//BoxCollider2D.size.y = BoxCollider2D.size.y / 2;
+								bc.size = new Vector2(bc.size.x,crouchHeight);
+								bc.center = new Vector2(bc.center.x,-.25f);
+						} else {
+								isCrouched = false;
+								walkSpeed = crouchSpeed*2f;
+								bc.size = new Vector2(bc.size.x,1f);
+								bc.center = new Vector2(bc.center.x,0f);
+						}
+				}
 		}
-	}
-	
 	void FixedUpdate () {
 		float inputX = Input.GetAxis (horizAxisName);
 		float vel = inputX * runSpeed;
