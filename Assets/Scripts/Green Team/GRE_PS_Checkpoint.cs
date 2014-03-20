4d2e7b37f7a6b56f0906235e58a150eaab7d297c
psteinbach <psteinba@trinity.edu>
2014-03-20 16:57:57 -0500

    Checkpoint Materials - Work in Progress
    Don't use these materials as of yet - currently in need of work.

---
+++ Assets/Scripts/Green Team/GRE_PS_Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GRE_PS_Checkpoint : MonoBehaviour {
+	public Vector2 pos = new Vector2(1.30785f, 0.10806f); //Need to make a global spawn variable for each level
+	void OnTriggerEnter2D(Collider2D playerCollision)
+	{
+		Debug.Log("Wut");
+		if (playerCollision.tag == "DeathArea") {
+			gameObject.transform.position = pos;
+		} 
+		else if (playerCollision.tag == "Checkpoint") {
+			pos = transform.position;
+		}
+	}
+}
