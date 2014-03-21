aabacc348d8f3b578d8b2e7067c9a11b6d6d3400
bpauls <bpauls@trinity.edu>
2014-03-21 01:17:21 -0500

    BossLion Update
    Worked on scripting and modeling behavior after flippers.

--- Assets/Scenes/Red Team/Scripts/BossLionBehavior.cs
+++ Assets/Scenes/Red Team/Scripts/BossLionBehavior.cs
@@ -1,15 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BossLionBehavior : MonoBehaviour {
 
+
+	bool left = false;
+	bool right = false;
+	int fallblock = rigidbody2D.velocity.y;
+
 	// Use this for initialization
 	void Start () {
+
+		
+	void OnTriggerEnter2D (Collider2D other) {
+			if (other.gameObject.rigidbody2D.mass >= 1){
+				fallblock == transform.ImagePosition.y;
+
+				while (fallblock == transform.ImagePosition.y ){
+					rigidbody2D.velocity.x = rigidbody2D.velocity.x*-1;
+
+
+				}
+			}
+		}
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 }
