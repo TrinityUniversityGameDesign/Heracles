25b77f691343485653d8e536d6238638fa81f47e
bpauls <bpauls@trinity.edu>
2014-03-23 22:25:49 -0500

    BossLion Tracking Movement Implemented

--- Assets/Scenes/Red Team/Scripts/BossLionBehavior.cs
+++ Assets/Scenes/Red Team/Scripts/BossLionBehavior.cs
@@ -3,42 +3,57 @@ using System.Collections;
 
 public class BossLionBehavior : MonoBehaviour {
 
-
-
-
-	/*
-	GameObject greenpaw = gameObject.transform.Find("greenpaw").gameObject;
-
-	GameObject redpaw = gameObject.transform.Find("redpaw").gameObject;
-
-	GameObject lion = gameObject.transform.Find("BossLion").gameObject;
-
-
-
-	// Use this for initialization
+	 Transform target;
+	public Transform enemyTransform;
+	public float speed = 3f;
+	public float rotationSpeed=10f;
+	Vector3 upAxis = new Vector3 (0f, 0f, 1f);
+	
 	void Start () {
-
-		redpaw.Sprite = false;
-		greenpaw.Sprite = false;
-
-	}
-
-
-
-	void OnTriggerEnter2D(Collider2D otherCollider)
-	{
-		currentpos = lion.Vector2;
-		lion.Vector2.MoveTowards (currentpos, redpaw.Vector2, 3);
-
-	}
-
-	/*void OnTriggerEnter2D(Collider2D otherCollider)
-	{
-		currentpos = lion.Vector2;
-		lion.Vector2.MoveTowards (currentpos, greenpaw.Vector2, 3);
 		
 	}
-      */
+	
+	void FixedUpdate(){
+		
+		target = GameObject.FindWithTag ("P1").transform;
+	}
+	
+	void Update(){
+		//rotate to look at the player
+		
+		transform.LookAt (target.position, upAxis);
+		transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.z); 
 
+		;
+		
+		
+		
+		//move towards the player
+	
+		if (enemyTransform.position.x < target.position.x ) {
+
+			float x = enemyTransform.position.x;
+
+
+			enemyTransform.position =  new Vector3( x + (1*speed * Time.deltaTime),enemyTransform.position.y, enemyTransform.position.z);
+		}
+
+		if (enemyTransform.position.x > target.position.x) {
+		
+			float x = enemyTransform.position.x;
+			enemyTransform.position =  new Vector3( x + (-1*speed * Time.deltaTime),enemyTransform.position.y, enemyTransform.position.z);
+		}
+		//if on top of player
+		if (enemyTransform.position.x == target.position.x) {
+			enemyTransform.position = enemyTransform.position;
+		}
+
+		if(enemyTransform.position.x == target.position.x && enemyTransform.position.y == target.position.y)
+		{
+
+		}
+
+
+	}
 
 }
