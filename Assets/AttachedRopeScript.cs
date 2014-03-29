2d7c074db0a3503f9272e03e31c435a1b508b234
kingofferrets <cfisher@trinity.edu>
2014-03-29 17:44:38 -0500

    Moving platforms are ground, and updating Labor12 with Labor12B
    DoricBridge is just a Doric platform, but with a different center of
    rotation so I can do something properly.

---
+++ Assets/AttachedRopeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachedRopeScript : MonoBehaviour {
+
+
+	public GameObject rope;
+	private Transform pos;
+	// Use this for initialization
+	void Start () {
+		pos = this.gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pos.eulerAngles.z > 10) {
+			pos.Rotate (new Vector3 (0f, 0f, 0.5f));
+		}
+	}
+}
