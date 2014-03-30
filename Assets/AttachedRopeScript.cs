077088482fc177d0975fab4917a1bfb34124990f
kingofferrets <cfisher@trinity.edu>
2014-03-29 19:00:36 -0500

    Further Labor 12B
    Ropes working, omg

--- Assets/AttachedRopeScript.cs
+++ Assets/AttachedRopeScript.cs
@@ -5,15 +5,17 @@ public class AttachedRopeScript : MonoBehaviour {
 
 
 	public GameObject rope;
+	private RopeScript ropeS;
 	private Transform pos;
 	// Use this for initialization
 	void Start () {
 		pos = this.gameObject.GetComponent<Transform>();
+		ropeS = rope.GetComponent<RopeScript> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (pos.eulerAngles.z > 10) {
+		if (pos.eulerAngles.z > 10 && ropeS.severed) {
 			pos.Rotate (new Vector3 (0f, 0f, 0.5f));
 		}
 	}
---
+++ Assets/RopeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeScript : MonoBehaviour {
+
+    public bool severed;
+	// Use this for initialization
+	void Start () {
+        severed = false;
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+        if (shot != null)
+        {
+            severed = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
