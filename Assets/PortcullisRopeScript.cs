e056ff1ecbfc614507092e424418f44cde78a2d6
kingofferrets <cfisher@trinity.edu>
2014-03-29 19:53:22 -0500

    Death zone glitch fix and labor 12B
    Death zones now make sure that things are players before sending them
    back.

---
+++ Assets/PortcullisRopeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortcullisRopeScript : MonoBehaviour {
+
+    public GameObject rope;
+    private RopeScript ropeS;
+    private Transform pos;
+    // Use this for initialization
+    void Start()
+    {
+        pos = this.gameObject.GetComponent<Transform>();
+        ropeS = rope.GetComponent<RopeScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ropeS.severed)
+        {
+            pos.position = new Vector3(pos.position.x, pos.position.y - 0.1f, pos.position.z);
+        }
+    }
+}
--- Assets/Scripts/Green Team/GRE_PS_Checkpoint.cs
+++ Assets/Scripts/Green Team/GRE_PS_Checkpoint.cs
@@ -6,11 +6,16 @@ public class GRE_PS_Checkpoint : MonoBehaviour {
 	public static Vector2 respawnPos = new Vector2(); //Need to make a global spawn variable for each level
 	void OnTriggerEnter2D(Collider2D playerCollision)
 	{
-		if (gameObject.tag == "DeathArea") {
-			playerCollision.transform.position = respawnPos;
-	} 
-		if (gameObject.tag == "Checkpoint") {
-			respawnPos = playerCollision.transform.position; //Not resetting global variable
-		}
+        if (playerCollision.gameObject.tag == "P1")
+        {
+            if (gameObject.tag == "DeathArea")
+            {
+                playerCollision.transform.position = respawnPos;
+            }
+            if (gameObject.tag == "Checkpoint")
+            {
+                respawnPos = playerCollision.transform.position; //Not resetting global variable
+            }
+        }
 	}
 }
