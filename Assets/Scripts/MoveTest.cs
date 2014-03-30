c8918f7cf6ffdc237004edbc9f560f3f47d63dc4
cjpeake9 <cpeake@trinity.edu>
2014-03-30 13:17:13 -0500

    commented MoveTest to better describe usage

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -8,7 +8,47 @@ public class MoveTest2 : MonoBehaviour {
 	public string easetype;
 	public string looptype;
 
-
+	/*Looptypes:
+	 *	loop - will perfrom the animation in a looping fassion, starting each time from the beginning of the path
+	 *	pingpong - will play the animation forward following the path and then retrace the path backwards then loop
+	 *Eastypes:
+	 *	the most commonly used are:
+	 *		linear - constant motion throughout the animation
+	 *		easeInOutSine - animation slows towards the begging and end speeding up slightly in the middle
+	 *  others:
+	 *		easeInQuad
+	 *		easeOutQuad
+	 *		easeInOutQuad
+	 *		easeInCubic
+	 *		easeOutCubic
+	 *		easeInOutCubic
+	 *		easeInQuart
+	 *		easeOutQuart
+	 *		easeInOutQuart
+	 *		easeInQuint
+	 *		easeOutQuint
+	 *		easeInOutQuint
+	 *		easeInSine
+	 *		easeOutSine
+	 *		easeInExpo
+	 *		easeOutExpo
+	 * 		easeInOutExpo
+	 *		easeInCirc
+	 *		easeOutCirc
+	 *		easeInOutCirc
+	 *		spring
+	 *		easeInBounce
+	 *		easeOutBounce
+	 *		easeInOutBounce
+	 *		easeInBack
+	 *		easeOutBack
+	 *		easeInOutBack
+	 *		easeInElastic
+	 *		easeOutElastic
+	 *		easeInOutElastic
+	 *		
+	 *		additional doccumentation can be found at http://itween.pixelplacement.com/documentation.php
+	 */
 	// Use this for initialization
 	void Start () {
 		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath (pathName), "time", time, "looptype", looptype, "easetype", easetype, "delay", delay));
