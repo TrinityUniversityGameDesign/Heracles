bf9354eafd417523c234bb1b4ed2409df552a09d
kingofferrets <cfisher@trinity.edu>
2014-03-30 14:09:35 -0500

    Fireballs, fireball spawners, and labor 12B
    Have everything in Labor 12B done except for Cerberus. Woo! Fireball
    spawners are neat, you can use them if you want. Designed to alternate
    between crouch and jump.

---
+++ Assets/FireballScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballScript : MonoBehaviour {
+    public int life; //How long the fireball lasts
+    public float speed; //How fast it goes
+    private int counter; //How long the fireball has been alive
+    public Transform pos; //The fireball's transform
+
+    void start()
+    {
+        counter = 0;
+    }
+
+	void FixedUpdate () {
+        if (counter > life) //If fireball's life is over, destroy it
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            counter++;
+            if (pos != null) //for some reason, Pos has to be public, rather than just getting the component. I don't know why.
+            {
+                pos.Translate(new Vector3(speed, 0, 0));
+            }
+        }
+	}
+}
---
+++ Assets/FireballSpawnScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballSpawnScript : MonoBehaviour {
+
+    public GameObject fireBall;
+    private bool active;
+    public int fireRate;
+    public int fireLife;
+    public float fireSpeed;
+    private int timer;
+	// Use this for initialization
+	void Start () {
+        active = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+        if (active)
+        {
+            if (timer >= fireRate)
+            {
+
+                GameObject newObj = Instantiate(fireBall) as GameObject;
+                if (Random.Range(0f, 1f) < 0.5) // 50/50 chance
+                { //comes from spawner's position
+                    newObj.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                }
+                else
+                { //comes down at your legs
+                    newObj.transform.position = new Vector3(transform.position.x, transform.position.y - 1.25f, transform.position.z);
+                }
+                FireballScript fire = newObj.GetComponent<FireballScript>();
+                fire.life = fireLife;
+                fire.speed = fireSpeed;
+                timer = 0;
+            }
+            timer++;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D playerCollision)
+	{
+        if (playerCollision.gameObject.tag == "P1") {
+            active = false; //the box collider is positioned in front, and turns it off when passed through
+        }
+    }
+}
