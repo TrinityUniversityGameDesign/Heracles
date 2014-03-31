0835f10dd3d91b3577a96ed17163e2b22b147738
tdwolf <twolf@trinity.edu>
2014-03-30 19:40:14 -0500

    Timer

---
+++ Assets/Scenes/BlueTeamLevel2/Scripts/TimeText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeText : MonoBehaviour {
+
+	public GUIText timeText;
+	public float timer = 0.00f; // initialize a timer that starts at 0.00
+	
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		timeText.text = "Time Elapsed " + timer.ToString ("0.0");
+	}
+}
