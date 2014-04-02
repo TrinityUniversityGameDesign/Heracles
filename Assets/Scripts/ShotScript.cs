be1df65ad001a2eefd92f185a6ed1f32a5571bca
Addison Freeman <afreeman@globalscape.com>
2014-04-02 13:16:59 -0500

    Arrow Hit Sound working

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -8,6 +8,9 @@ public class ShotScript : MonoBehaviour
 {
   public AudioClip arrowShoot1;
   public AudioClip arrowShoot2;
+  public AudioClip arrowHit1;
+  public AudioClip arrowHit2;
+  public AudioClip arrowHit3;  
   // 1 - Designer variables
 
   /// <summary>
@@ -25,10 +28,16 @@ public class ShotScript : MonoBehaviour
 	float randomNum = Random.Range (1, 100);
 	if (randomNum < 50) {
 	  audio.PlayOneShot (arrowShoot1);
+	  Debug.Log("arrowShoot1");
 	} else {
 	  audio.PlayOneShot (arrowShoot2);
+	  Debug.Log("arrowShoot2");
 	}
     // 2 - Limited time to live to avoid any leak
     Destroy(gameObject, 20); // 20sec
+  }
+  void OnTriggerEnter2D () 
+  {
+    audio.PlayOneShot (arrowHit1);
   }
 }
