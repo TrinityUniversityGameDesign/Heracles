502cdd94a913c861a61395e63bf8df704e14a17b
cunger1 <cunger1@trinity.edu>
2014-04-04 03:25:01 -0500

    Player Health

---
+++ Assets/Scripts/Green Team/playerHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHealth : MonoBehaviour {
+
+	public int health=2;
+
+	//private GameObject dude = new GameObject.FindGameObjectsWithTag("P1");
+	public static Vector2 respawnPos = new Vector2(21,2);
+
+	public void damagePlayer(int damage)
+	{
+		health-=damage;
+		if (health <= 0) {
+			Destroy (gameObject);
+			//dude.transform.position = respawnPos;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		projectile proj = otherCollider.gameObject.GetComponent<projectile>();
+		if (proj!= null)
+		{
+			damagePlayer(proj.damage);
+			Destroy(proj.gameObject); 
+		}
+	}
+}
---
+++ Assets/Scripts/Green Team/projectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile : MonoBehaviour {
+	
+	public int damage = 1;
+	public bool hit = false;
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 direction = new Vector2(1,0);
+	private Vector2 movement;
+	
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+		//if (gameObject.rigidbody2D.velocity.x < .01 && gameObject.rigidbody2D.velocity.y < .01)
+			//Destroy (gameObject); 
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+	}
+
+}
