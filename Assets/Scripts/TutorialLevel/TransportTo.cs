63c3188ab62e3ab3be97ce10432575492934d898
John Mendiola <johnnywendy@gmail.com>
2014-04-09 14:18:49 -0700

    Added the black area in the Tutorial (step 1)
    Teaches death, shooting mechanics. Created the TransportTo script that
    causes player to teleport to chosen position.

---
+++ Assets/Scripts/TutorialLevel/TransportTo.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransportTo : MonoBehaviour
+{
+
+		GameObject p;
+		GameObject c;
+		public float x, y, z;
+		public float cameraX, cameraY, cameraZ;
+		public bool oneUse;
+		bool used = false;
+		public bool triggeredByPlayer;
+		public int triggeredOnNumTry;
+
+		// Use this for initialization	
+		void Start ()
+		{
+				p = GameObject.FindGameObjectWithTag ("P1");
+				c = GameObject.FindGameObjectWithTag ("MainCamera");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+
+		}
+
+		void OnTriggerEnter2D (Collider2D other)
+		{
+				if (!used) {
+						if (triggeredByPlayer) {
+								if (other.CompareTag ("P1")) {
+										if (triggeredOnNumTry == 0) {
+												p.transform.position = new Vector3 (x, y, z);
+												c.transform.position = new Vector3 (cameraX, cameraY, cameraZ);
+												if (oneUse)
+														used = true;
+										} else {
+												triggeredOnNumTry--;
+										}
+								}
+						}
+						if (other.CompareTag ("Shot")) {
+								if (triggeredOnNumTry == 0) {
+										p.transform.position = new Vector3 (x, y, z);
+										c.transform.position = new Vector3 (cameraX, cameraY, cameraZ);
+										if (oneUse)
+												used = true;
+								} else {
+										triggeredOnNumTry--;
+								}
+						}
+				}
+		}
+}
