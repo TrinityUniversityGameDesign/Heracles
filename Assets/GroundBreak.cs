3bb2a661a5047c9c394e713595051fde82f47d19
kingofferrets <cfisher@trinity.edu>
2014-04-09 11:22:11 -0500

    BreakableGround
    It works.

---
+++ Assets/GroundBreak.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundBreak : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		if (otherCollider.gameObject.tag == "P1") {
+			Destroy(this.gameObject);
+		}
+	}
+}
