576e766f1ee29d734c2a2f3a44003f410dc9fea0
John Mendiola <johnnywendy@gmail.com>
2014-04-16 03:15:26 -0500

    Put all the levels together
    There is now a scene changer in every level. Also, added controls to
    scene changer and labels.

--- Assets/Scenes/Red Team/Scripts/PlayerScene.cs
+++ Assets/Scenes/Red Team/Scripts/PlayerScene.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneChange : MonoBehaviour {
+public class PlayerScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
