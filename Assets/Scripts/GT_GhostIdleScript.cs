e7c00efe7543966892c70b63671dc1ca04b01b67
msaxon <msaxon@trinity.edu>
2014-04-21 13:12:42 -0500

    Added Stationary Ghosts

---
+++ Assets/Scripts/GT_GhostIdleScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GT_GhostIdleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 transScale = transform.localScale;
+		transScale.x *= -1;
+		transform.localScale = transScale;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
