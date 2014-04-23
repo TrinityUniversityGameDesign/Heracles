e1a2ddf95fc1e22cbd61d85863e6270b90f01598
kingofferrets <cfisher@trinity.edu>
2014-04-23 13:15:10 -0500

    Fixed tons of Labor12B errors, breakable ground no longer has arrow collision on invisible hitbox

--- Assets/GroundBreak.cs
+++ Assets/GroundBreak.cs
@@ -6,7 +6,7 @@ public class GroundBreak : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
 		if (otherCollider.gameObject.tag == "P1") {
-			Destroy(this.gameObject);
+			Destroy(this.gameObject.transform.parent.gameObject);
 		}
 	}
 }
