7daf4d2a52ab92c08f5c7b7c0dc55f124aefbfec
rsteup <rsteup@trinity.edu>
2014-04-27 17:58:57 -0500

    Implementing Alternative Health GUI
    -deactivated playerHealth script in Artemis boss fight
    -added optional health GUI that is compatible with BT_HealthScript
    (adapted code from playerHealth)

---
+++ Assets/Scenes/BlueTeamLevel2/Scripts/BT_HealthGUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BT_HealthGUI : MonoBehaviour {
+
+	public Texture2D healthUnit;
+	private BT_HealthScript healthScript;
+
+	void OnGUI() {
+		int currentHealth = healthScript.GetHealth ();
+		Debug.Log ("current health: " + currentHealth.ToString ());
+		int space = 50;
+		for (int i=1; i <= currentHealth; i++)
+		{
+			GUI.DrawTexture (new Rect((20 + space*(i-1)),20,50,25), healthUnit, ScaleMode.ScaleToFit);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//healthUnit = (Texture2D)Resources.LoadAssetAtPath("Resources/TEMPheart.png", typeof(Sprite));
+		healthScript = GetComponent<BT_HealthScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
