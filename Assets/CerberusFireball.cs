cc5b92276e5bd82ddbb88410e3912094f96bcad4
kingofferrets <cfisher@trinity.edu>
2014-04-28 14:16:46 -0500

    Cerberus updates

---
+++ Assets/CerberusFireball.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CerberusFireball : MonoBehaviour {
+	public float speed; //How fast it goes
+	private Transform pos; //The fireball's transform
+	public Vector3 initDest;
+	private bool hitDest;
+
+	// Use this for initialization
+	void Start () {
+		hitDest = false;
+		pos = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (hitDest) {
+			moveToDestination();
+		} else {
+			headRight();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		if (otherCollider.tag == "ShotDestroyer") {
+			Destroy(this.gameObject);
+		}
+	}
+
+	void moveToDestination() {
+		pos.position = Vector3.MoveTowards (pos.position, initDest, speed);
+	}
+
+	void headRight() {
+		pos.position = new Vector3 (pos.position.x + speed, pos.position.y, 0);
+	}
+}
