4a8ceef03a84d5e31e0c103b7590e0e063069331
John Mendiola <johnnywendy@gmail.com>
2014-05-01 02:37:19 -0500

    (Tutorial) Oops on TransportTo Script
    Oops, my last changes broke it. I deleted the fade code I added. I did
    also delete extra fields and parts of code now unnecessary.

--- Assets/Scripts/TutorialLevel/TransportTo.cs
+++ Assets/Scripts/TutorialLevel/TransportTo.cs
@@ -10,17 +10,9 @@ public class TransportTo : MonoBehaviour
 		public bool oneUse;
 		bool used = false;
 		public bool triggeredByPlayer;
-		public float seconds;
-		public GameObject deathFader;
-		private GameObject deathfader;
-		private bool fade = false;
 
-		// Use this for initialization	
 		void Start ()
 		{
-				if (seconds != null) {
-						seconds = 1.4f;		
-				}
 				p = GameObject.FindGameObjectWithTag ("P1");
 				c = GameObject.FindGameObjectWithTag ("MainCamera");
 		}
@@ -28,11 +20,7 @@ public class TransportTo : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-				if (fade) {
-						Color tempColor = deathfader.GetComponent<MeshRenderer> ().material.color;
-						tempColor.a += 0.015f;
-						deathfader.GetComponent<MeshRenderer> ().material.color = tempColor;
-				}
+
 		}
 
 		void OnTriggerEnter2D (Collider2D other)
@@ -40,14 +28,6 @@ public class TransportTo : MonoBehaviour
 				if (!used) {
 						if (triggeredByPlayer) {
 								if (other.tag == "P1") {
-										/*
-										StartCoroutine (wait (seconds));
-										deathfader = Instantiate (deathFader, other.transform.position, Quaternion.identity) as GameObject;
-										Color tempColor = deathfader.GetComponent<MeshRenderer> ().material.color;
-										tempColor.a = 0.0f;
-										deathfader.GetComponent<MeshRenderer> ().material.color = tempColor;
-										fade = true;
-										*/
 										p.transform.position = new Vector3 (x, y, z);
 										c.transform.position = new Vector3 (x, y, z - 63f);
 										Camera.main.GetComponent<CameraFollow> ().SetTarget (p.transform);
@@ -55,14 +35,6 @@ public class TransportTo : MonoBehaviour
 												used = true;
 								}
 						} else if (other.CompareTag ("Shot")) {
-								/*
-								StartCoroutine (wait (seconds));
-								deathfader = Instantiate (deathFader, other.transform.position, Quaternion.identity) as GameObject;
-								Color tempColor = deathfader.GetComponent<MeshRenderer> ().material.color;
-								tempColor.a = 0.0f;
-								*/
-								deathfader.GetComponent<MeshRenderer> ().material.color = tempColor;
-								fade = true;
 								p.transform.position = new Vector3 (x, y, z);
 								c.transform.position = new Vector3 (x, y, z - 63f);
 								Camera.main.GetComponent<CameraFollow> ().SetTarget (p.transform);
@@ -71,15 +43,5 @@ public class TransportTo : MonoBehaviour
 
 						}
 				}
-		}
-
-		IEnumerator wait (float seconds)
-		{
-				yield return new WaitForSeconds (seconds); 
-				Color tempColor = deathfader.GetComponent<MeshRenderer> ().material.color;
-				tempColor.a = 0.0f;
-				deathfader.GetComponent<MeshRenderer> ().material.color = tempColor;
-				fade = false;
-
 		}
 }
