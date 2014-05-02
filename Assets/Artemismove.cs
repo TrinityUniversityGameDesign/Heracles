42b0e023aca60e28514d61841565495c413e75b9
hmcdona1 <gmail@reecemcdonald.com>
2014-05-02 12:03:58 -0500

    Artemis Difficulty Ramp
    - Artemis teleports and shoots faster as the hinds health goes down
    - Also stops flying when the player dies

--- Assets/Artemismove.cs
+++ Assets/Artemismove.cs
@@ -9,19 +9,43 @@ public class Artemismove : MonoBehaviour {
 
 	private Animator anim;
 	private bool facingRight = true;
+	private float difficultyMod = 1;
+	private GameObject hind;
+	private bool canShoot = true;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
 		StartCoroutine(WaitAndShoot(3f));
+		hind = GameObject.Find("GoldenHindRider");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.FindGameObjectWithTag("P1").transform.position.x < transform.position.x && facingRight)
-			flipDirection();
-		if (GameObject.FindGameObjectWithTag("P1").transform.position.x > transform.position.x && !facingRight)
-			flipDirection();
+		if (GameObject.FindGameObjectsWithTag("P1").Length == 0) {
+			canShoot = false;
+			anim.SetBool("playerDead",true);
+			rigidbody2D.gravityScale = 0.5f;
+		}
+		if (canShoot) {
+			if (GameObject.FindGameObjectWithTag("P1").transform.position.x < transform.position.x && facingRight)
+				flipDirection();
+			if (GameObject.FindGameObjectWithTag("P1").transform.position.x > transform.position.x && !facingRight)
+				flipDirection();
+			if (hind.GetComponent<BT_HealthScript>().GetHealth() < 3)
+				difficultyMod = 1.5f;
+			if (hind.GetComponent<BT_HealthScript>().GetHealth() < 3)
+				difficultyMod = 2f;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Main Stage") {
+			GetComponent<BoxCollider2D>().isTrigger = false;
+			rigidbody2D.velocity = Vector3.zero;
+			anim.SetBool("grounded",true);
+			rigidbody2D.gravityScale = 0f;
+		}
 	}
 
 	void MoveArtemis () {
@@ -41,27 +65,39 @@ public class Artemismove : MonoBehaviour {
 		transform.localScale = transScale;
 	}
 
+	// Starts shooting Animation
 	IEnumerator WaitAndShoot(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
-		anim.SetTrigger("doShoot");
-		StartCoroutine(Shoot(0.4f));
+		if (canShoot) {
+			anim.SetTrigger("doShoot");
+			StartCoroutine(Shoot(0.4f));
+		}
 	}
 
+	// Shoots arrow, starts waiting to start teleport animation
 	IEnumerator Shoot(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
-		GetComponent<BT_ObjGeneratorScript>().Generate();
-		StartCoroutine(WaitAndTeleport(1f));
+		if (canShoot) {
+			GetComponent<BT_ObjGeneratorScript>().Generate();
+			StartCoroutine(WaitAndTeleport(1.5f/difficultyMod));
+		}
 	}
 
+	// Starts teleport animation
 	IEnumerator WaitAndTeleport(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
-		anim.SetTrigger("doTeleport");
-		StartCoroutine(WaitAndMove(0.4f));
+		if (canShoot) {
+			anim.SetTrigger("doTeleport");
+			StartCoroutine(WaitAndMove(0.4f));
+		}
 	}
 
+	// Move Artemis, waits to shoot again
 	IEnumerator WaitAndMove(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
-		MoveArtemis();
-		StartCoroutine(WaitAndShoot(2.2f));
+		if (canShoot) {
+			MoveArtemis();
+			StartCoroutine(WaitAndShoot(2.6f/difficultyMod));
+		}
 	}
 }
