89812882dde50b1ed8a2e03f2966ef01e6a3ad5b
cunger1 <cunger1@trinity.edu>
2014-05-08 08:03:48 -0500

    Health bar changes - incomplete

--- Assets/Scripts/Green Team/playerHealth.cs
+++ Assets/Scripts/Green Team/playerHealth.cs
@@ -6,7 +6,10 @@ public class playerHealth : MonoBehaviour {
 	public int maxHealth = 2;
 	public int currenthealth=2;
 	//public Texture2D heart = Resources.Load("TEMPheart") as Texture2D;
-	public Texture2D heart = (Texture2D)Resources.LoadAssetAtPath("Resources/TEMPheart.png", typeof(Sprite));
+	public Texture2D heart = (Texture2D)Resources.LoadAssetAtPath("Sprites/TEMPheart.png", typeof(Sprite));
+	public Texture2D healthEnd = (Texture2D)Resources.LoadAssetAtPath("Sprites/stringEnd.png", typeof(Sprite));
+	public Texture2D healthMiddle = (Texture2D)Resources.LoadAssetAtPath("Sprites/stringMiddle.png", typeof(Sprite));
+	public Texture2D healthStart = (Texture2D)Resources.LoadAssetAtPath("Sprites/scissorsOpen.png", typeof(Sprite));
 	//private GameObject dude = new GameObject.FindGameObjectsWithTag("P1");
 	public static Vector2 respawnPos = new Vector2(21,2);
 
@@ -33,6 +36,18 @@ public class playerHealth : MonoBehaviour {
 		if(currenthealth>=1) GUI.DrawTexture (new Rect(20,20,50,25), heart, ScaleMode.ScaleToFit);
 		if(currenthealth>=2) GUI.DrawTexture (new Rect(70,20,50,25), heart, ScaleMode.ScaleToFit);
 		if(currenthealth>=3) GUI.DrawTexture (new Rect(120,20,50,25), heart, ScaleMode.ScaleToFit);
+
+		/*if(currenthealth>=1) GUI.DrawTexture (new Rect(20,20,50,25), healthEnd, ScaleMode.ScaleToFit);
+		if(currenthealth==2) GUI.DrawTexture (new Rect(70,20,50,25), healthStart, ScaleMode.ScaleToFit);
+		if(currenthealth>2) GUI.DrawTexture (new Rect(70,20,50,25), healthMiddle, ScaleMode.ScaleToFit);
+		if(currenthealth==3) GUI.DrawTexture (new Rect(120,20,50,25), healthStart, ScaleMode.ScaleToFit);
+		if(currenthealth>3) GUI.DrawTexture (new Rect(120,20,50,25), healthMiddle, ScaleMode.ScaleToFit);
+		if(currenthealth==4) GUI.DrawTexture (new Rect(170,20,50,25), healthStart, ScaleMode.ScaleToFit);
+		if(currenthealth>4) GUI.DrawTexture (new Rect(170,20,50,25), healthMiddle, ScaleMode.ScaleToFit);*/
+		//if(currenthealth==5) GUI.DrawTexture (new Rect(70,20,50,25), healthStart, ScaleMode.ScaleToFit);
+		//if(currenthealth>5) GUI.DrawTexture (new Rect(70,20,50,25), healthMiddle, ScaleMode.ScaleToFit);
+		//if(currenthealth==6) GUI.DrawTexture (new Rect(70,20,50,25), healthStart, ScaleMode.ScaleToFit);
+		//if(currenthealth>6) GUI.DrawTexture (new Rect(70,20,50,25), healthMiddle, ScaleMode.ScaleToFit);
 	}
 
 	// Update is called once per frame
